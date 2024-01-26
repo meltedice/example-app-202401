592594d6e2a0e4368bb98fb625d671da6a40fc9b
ice <ise.yohei@classmethod.jp>
2024-01-26 22:03:20 +0900

    replace HomePage to Notifications' page

--- src/components/pages/HomePage/HomePage.tsx
+++ src/components/pages/HomePage/HomePage.tsx
@@ -1,38 +1,19 @@
-import { useState } from 'react'
-// eslint-disable-next-line import/no-absolute-path
-import viteLogo from '/vite.svg'
-import reactLogo from '@/assets/react.svg'
 import { useGetNotifications } from '@/queries/useGetNotifications'
 
 export function HomePage() {
-  const [count, setCount] = useState(0)
-  const { notifications } = useGetNotifications()
+  const { notifications, isLoading } = useGetNotifications()
 
-  // eslint-disable-next-line no-console
-  console.debug('notifications:', notifications)
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <ul>
+        {notifications.map((notification) => {
+          return <li key={notification.id}>{notification.title}</li>
+        })}
+      </ul>
+    </div>
   )
 }
--- src/queries/useGetNotifications.ts
+++ src/queries/useGetNotifications.ts
@@ -6,14 +6,23 @@ export interface Notification {
   title: string
 }
 
+interface GetNotificationsResponse {
+  notifications: Notification[]
+}
+
 export function useGetNotifications() {
-  const { data, isLoading, error } = useQuery<unknown, Error, Notification>({
+  const { data, isLoading, error } = useQuery<
+    unknown,
+    Error,
+    GetNotificationsResponse
+  >({
     queryKey: ['GET_NOTIFICATIONS'],
     queryFn: async () => {
       const res = await fetch('/notifications')
       return res.json()
     },
   })
+  const { notifications } = data ?? { notifications: [] }
 
-  return { notifications: data, isLoading, error }
+  return { notifications, isLoading, error }
 }
