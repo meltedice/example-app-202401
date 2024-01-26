3f5f8916db51c4a0ea58e0c3f4ce5d1ebf485984
ice <ise.yohei@classmethod.jp>
2024-01-26 21:13:07 +0900

    delegate App.tsx design into HomePage.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,8 @@
-import { useState } from 'react'
-// eslint-disable-next-line import/no-absolute-path
-import viteLogo from '/vite.svg'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import { HomePage } from './components/pages/HomePage'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  return <HomePage />
 }
 
 // eslint-disable-next-line import/no-default-export
